66c88cc7451cfe6681550d0dd7bd6976c2e40d79
longtuanvu26 <tuanlonggg26@gmail.com>
2025-03-27 12:44:47 +0700

    updatecode

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,1 +1,50 @@
-import '../../core/api/api_client.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'slide_screen.dart'; // Import màn hình slide
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Chuyển màn hình sau 3 giây với hiệu ứng mờ dần
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          transitionDuration: const Duration(milliseconds: 500),
+          pageBuilder: (context, animation, secondaryAnimation) =>
+          const SlideScreen(),
+          transitionsBuilder:
+              (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Image.asset(
+          'assets/images/anh5.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
