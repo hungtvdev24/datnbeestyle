b779ddfc8e328c7ffb8fc1f6cc9d73543205a97d
MeoXui <130195222+MeoXui@users.noreply.github.com>
2025-04-06 11:38:51 +0700

    Update checkout_provider.dart

--- lib/providers/checkout_provider.dart
+++ lib/providers/checkout_provider.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/cupertino.dart';
+
 import '../../core/api/api_client.dart';
+import '../core/api/checkout_service.dart';
+class CheckoutProvider extends ChangeNotifier {
+  bool isLoading = false;
+  String? errorMessage;
+  Map<String, dynamic>? orderData;
+
+  Future<void> placeOrder({
+    required String token,
+    required int idDiaChi,
+    required String phuongThucThanhToan,
+    required List<Map<String, dynamic>> selectedItems,
+    String? message,
+  }) async {
+    isLoading = true;
+    errorMessage = null;
+    orderData = null;
+    notifyListeners();
+
+    try {
+      final response = await CheckoutService.placeOrder(
+        token: token,
+        idDiaChi: idDiaChi,
+        phuongThucThanhToan: phuongThucThanhToan,
+        selectedItems: selectedItems,
+        message: message,
+      );
+
+      if (response.containsKey('error')) {
+        errorMessage = response['error'];
+      } else {
+        orderData = response;
+      }
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
